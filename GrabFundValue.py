6df5ad325eabe0a12ef4e8d88480a95f5a4b80bf
NetBeen <netbeen.cn@gmail.com>
2016-01-10 19:22:12 +0800

    拆分爬虫函数为单独文件，添加data范例

---
+++ GrabFundValue.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import datetime
+import time
+import urllib.request
+
+def grabFundValue(fundIDString, startTimeStr):
+    print("开始抓取基金净值")
+    urlString = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code='+fundIDString+'&page=1&per=3'
+    response = urllib.request.urlopen(urlString)
+    responseString = response.read().decode('gb2312')   
+    tbodyIndex = responseString.index('<tbody>')
+    tbodyEndIndex = responseString.index('</tbody>')
+    responseString = responseString[tbodyIndex+7:tbodyEndIndex]
+
+    eachRowList = responseString.split('</tr><tr>')     #使用制表分隔符切割
+    eachRowList[0] = eachRowList[0][4:]
+    eachRowList[-1] = eachRowList[-1][:-5]
+    eachRowList.reverse()   #逆置list，使日期递增
+
+    fundInfoDictList = []
+
+    for i in range(len(eachRowList)):   #对每一行进行拆分，提出净值和日期
+        thisRow = eachRowList[i]
+        thisRowSplitList = thisRow.split('</td><td')
+        thisRowSplitList.pop()
+        for j in range(len(thisRowSplitList)):
+            endIndex = thisRowSplitList[j].index('>')
+            thisRowSplitList[j] = thisRowSplitList[j][endIndex+1:]
+        fundInfoDict = {}
+        fundInfoDict['dateString'] = thisRowSplitList[0]
+        fundInfoDict['value'] = float(thisRowSplitList[1])
+        fundInfoDictList.append(fundInfoDict)
+        #print(thisRowSplitList)
+
+    print(fundInfoDictList)
+    print("抓取基金净值结束")
+    return
