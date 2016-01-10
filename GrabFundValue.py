17e9b17c96ad864dec0972d27b064677c79767aa
NetBeen <netbeen.cn@gmail.com>
2016-01-10 23:01:04 +0800

    添加模板文件，完成持仓成本计算

--- GrabFundValue.py
+++ GrabFundValue.py
@@ -5,9 +5,9 @@ import datetime
 import time
 import urllib.request
 
-def grabFundValue(fundIDString, startTimeStr):
-    print("开始抓取基金净值")
-    urlString = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code='+fundIDString+'&page=1&per=3'
+def grabFundValue(fundIDString, startDateStr):
+    print("开始抓取基金净值，基金编号：",fundIDString,' 起始时间：',startDateStr)
+    urlString = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code='+fundIDString+'&page=1&per=20000'
     response = urllib.request.urlopen(urlString)
     responseString = response.read().decode('gb2312')   
     tbodyIndex = responseString.index('<tbody>')
@@ -32,8 +32,15 @@ def grabFundValue(fundIDString, startTimeStr):
         fundInfoDict['dateString'] = thisRowSplitList[0]
         fundInfoDict['value'] = float(thisRowSplitList[1])
         fundInfoDictList.append(fundInfoDict)
-        #print(thisRowSplitList)
+    
+    startDateIndex = -1
+    for i in range(len(fundInfoDictList)):
+        if fundInfoDictList[i]['dateString'] == startDateStr:
+            startDateIndex = i
+            break
+    
+    if startDateIndex == -1:
+        print('错误：未找到与data文件起始日期相符的净值信息！')
 
-    print(fundInfoDictList)
     print("抓取基金净值结束")
-    return
+    return fundInfoDictList[startDateIndex:]
--- main.py
+++ main.py
@@ -10,5 +10,21 @@ if(__name__=='__main__'):
     currentDateString = time.strftime("%Y-%m-%d",time.localtime(time.time()))
     print("当前时间：",currentDateString)
 
-    loadPurchaseData()
-    grabFundValue('160119',currentDateString)
+    purchaseInfoDictListDict = loadPurchaseData()
+
+    for (key,value) in purchaseInfoDictListDict.items():
+        print('开始处理',key)
+        fundInfoDictList = grabFundValue(key,value[0]['dateString'])
+        
+        thisFundPurchaseInfoDictList = purchaseInfoDictListDict[key]
+        currentCost = 0
+        currentShare = 0
+        nextPurchaseInfoDictIndex = 0
+        for fundInfoDict in fundInfoDictList:
+            if fundInfoDict['dateString'] == thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['dateString']:
+                currentCost += thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['totalPrice']
+                currentShare +=  thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['totalPrice']/fundInfoDict['value']
+                nextPurchaseInfoDictIndex += 1
+            fundInfoDict['unitPrice'] = currentCost / currentShare
+
+        print(fundInfoDictList)
