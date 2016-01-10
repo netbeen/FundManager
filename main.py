287a80fa6bf19f46eabd223680bb6a528f6d49e8
NetBeen <netbeen.cn@gmail.com>
2016-01-10 20:11:53 +0800

    完成购买数据的读取，并返回一个基金字典（信息列表（信息字典（时间，总价）））

--- main.py
+++ main.py
@@ -4,8 +4,11 @@
 import datetime
 import time
 from GrabFundValue import grabFundValue
+from LoadData import loadPurchaseData
 
 if(__name__=='__main__'):
     currentDateString = time.strftime("%Y-%m-%d",time.localtime(time.time()))
     print("当前时间：",currentDateString)
+
+    loadPurchaseData()
     grabFundValue('160119',currentDateString)
