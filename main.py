84a1840b736de97ea4ab9e8c35132ca68235fd88
shuda.yy <shuda.yy@alibaba-inc.com>
2016-08-09 21:30:34 +0800

    重构代码

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from LoadData import load_purchase_data
 from GenerateHtml import generate_html
 from ProfitRatePerYear import ProfitRatePerYearCalculator
 
-if(__name__=='__main__'):
+if (__name__ == '__main__'):
     current_date_string = time.strftime("%Y-%m-%d", time.localtime(time.time()))
     print("当前时间：", current_date_string)
 
@@ -15,9 +15,9 @@ if(__name__=='__main__'):
 
     for (key, value) in purchase_info_dict_list_dict.items():
         print('-------------------------')
-        print('开始处理',key)
-        fundInfoDictList = grabFundValue(key,value[0]['dateString'])
-        
+        print('开始处理', key)
+        fundInfoDictList = grabFundValue(key, value[0]['dateString'])
+
         thisFundPurchaseInfoDictList = purchase_info_dict_list_dict[key]
         currentCost = 0
         currentShare = 0
@@ -27,12 +27,14 @@ if(__name__=='__main__'):
                 pass
             elif fundInfoDict['dateString'] == thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['dateString']:
                 currentCost += thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['totalPrice']
-                currentShare += thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['totalPrice']/fundInfoDict['value']
+                currentShare += thisFundPurchaseInfoDictList[nextPurchaseInfoDictIndex]['totalPrice'] / fundInfoDict[
+                    'value']
                 nextPurchaseInfoDictIndex += 1
-            fundInfoDict['unitPrice'] = currentCost / currentShare  #计算每份持仓成本
+            fundInfoDict['unitPrice'] = currentCost / currentShare  # 计算每份持仓成本
 
         for fundInfoDict in fundInfoDictList:
-            fundInfoDict['profitRate'] = (fundInfoDict['value']-fundInfoDict['unitPrice']) / fundInfoDict['unitPrice']  #计算收益率
+            fundInfoDict['profitRate'] = (fundInfoDict['value'] - fundInfoDict['unitPrice']) / fundInfoDict[
+                'unitPrice']  # 计算收益率
 
         redeemFeeRate = 0.5 / 100
 
@@ -49,15 +51,15 @@ if(__name__=='__main__'):
             else:
                 untilTheDayPurchaseInfoDictList = thisFundPurchaseInfoDictList[0:nextPurchaseInfoDictIndex]
             calculator = ProfitRatePerYearCalculator(untilTheDayPurchaseInfoDictList)
-            
+
             currentCost = 0
-            for i in range(0,nextPurchaseInfoDictIndex):
+            for i in range(0, nextPurchaseInfoDictIndex):
                 currentCost += thisFundPurchaseInfoDictList[i]['totalPrice']
             currentProfitAddCost = (fundInfoDict['profitRate'] - redeemFeeRate) * currentCost + currentCost
-            fundInfoDict['profitRatePerYear'] =  calculator.calc(currentProfitAddCost,fundInfoDict['dateString'])
+            fundInfoDict['profitRatePerYear'] = calculator.calc(currentProfitAddCost, fundInfoDict['dateString'])
 
         print('结束时间：', current_date_string)
         print('总成本：', currentCost)
-        print('利润率：',round(fundInfoDictList[-1]['profitRate']*100,4),'%')
-        print('年化收益率: ',round(fundInfoDictList[-1]['profitRatePerYear']*100,4),'%')
+        print('利润率：', '%.4f' % (fundInfoDictList[-1]['profitRate'] * 100), '%')
+        print('年化收益率: ', '%.4f' % (fundInfoDictList[-1]['profitRatePerYear'] * 100), '%')
         generate_html(key, currentCost, fundInfoDictList[-1]['profitRate'], fundInfoDictList)
